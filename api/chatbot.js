032c3f018761b9c17e399dacd6b0bab85f05d2b6
kjw0106 <kjwkjw0106@gmail.com>
2026-07-06 17:41:56 +0900

    Update chatbot.js

--- api/chatbot.js
+++ api/chatbot.js
@@ -1,1 +1,19 @@
-
+// api/chatbot.js
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    res.status(200).json({
+      "version": "2.0",
+      "template": {
+        "outputs": [
+          {
+            "simpleText": {
+              "text": "안녕하세요! 가격 알림 챗봇입니다."
+            }
+          }
+        ]
+      }
+    });
+  } else {
+    res.status(405).send('Method Not Allowed');
+  }
+}
